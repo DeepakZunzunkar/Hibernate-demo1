ea05103ee70cc946b413f315824b3d7f80c32f8f
DeepakJavaDev <dzunzunkar@gmail.com>
2023-02-27 20:32:35 +0530

    change way of use for transaction

--- src/com/daoImpl/StudebtDaoImpl.java
+++ src/com/daoImpl/StudebtDaoImpl.java
@@ -25,102 +25,135 @@ public class StudebtDaoImpl
 	
 	public static int add()
 	{
-		student=new Student();
+		//A typical transaction should use the following idiom:
+		try {
+
+			student=new Student();
+			
+			System.out.println("Enter Name :");
+			student.setName(sc.next());
+			
+			System.out.println("Enter Address :");
+			student.setAddress(sc.next());
+			
+			session=sf.openSession();
+			tx=session.beginTransaction();
+			
+			//return last inserted record id
+			rs=(Integer)session.save(student);
+			
+			tx.commit();
 		
-		System.out.println("Enter Name :");
-		student.setName(sc.next());
+		}catch (Exception e) {
+		     if (tx!=null) tx.rollback();
+		     throw e;
+		}finally {
+			 session.close();
+			//sf.close();
+		 }
+		 
 		
-		System.out.println("Enter Address :");
-		student.setAddress(sc.next());
-		
-		session=sf.openSession();
-		tx=session.beginTransaction();
-		
-		//return last inserted record id
-		rs=(Integer)session.save(student);
-		
-		tx.commit();
-		
-		session.close();
-		//sf.close();
 		return rs;
 	}
 	public static int update()
 	{
-		student=new Student();
-		
-		System.out.println("You should Know ID :");
-		
-		System.out.println("Enter ID :");
-		student.setId(sc.nextInt());
-		
-		System.out.println("Enter Name :");
-		student.setName(sc.next());
-		
-		System.out.println("Enter Address :");
-		student.setAddress(sc.next());
-		
-		session=sf.openSession();
-		tx=session.beginTransaction();
-		
-		session.update(student);
-		
-		tx.commit();
-		
-		System.out.println("Record Updated Successfully..");
-		
-		session.close();
-		//sf.close();
+		try {
+			student=new Student();
+			
+			System.out.println("You should Know ID :");
+			
+			System.out.println("Enter ID :");
+			student.setId(sc.nextInt());
+			
+			System.out.println("Enter Name :");
+			student.setName(sc.next());
+			
+			System.out.println("Enter Address :");
+			student.setAddress(sc.next());
+			
+			session=sf.openSession();
+			tx=session.beginTransaction();
+			
+			session.update(student);
+			
+			tx.commit();
+			
+			System.out.println("Record Updated Successfully..");
+			
+		}catch (Exception e) {
+		     if (tx!=null) tx.rollback();
+		     throw e;
+		}finally {
+			 session.close();
+			//sf.close();
+		 }
 		
 		return 0;
 	}
 	
 	public static int  delete()
 	{
-		student=new Student();
-		
-		System.out.println("You should Know ID :");
-		
-		System.out.println("Enter ID :");
-		student.setId(sc.nextInt());
-		
-		session=sf.openSession();
-		tx=session.beginTransaction();
-		
-		session.delete(student);
-		
-		tx.commit();
-		System.out.println("Record Deleted Successfully..");
-		
-		session.close();
-		//sf.close();
-		
+		try {
+			student=new Student();
+			
+			System.out.println("You should Know ID :");
+			
+			System.out.println("Enter ID :");
+			student.setId(sc.nextInt());
+			
+			session=sf.openSession();
+			tx=session.beginTransaction();
+			
+			session.delete(student);
+			
+			tx.commit();
+			System.out.println("Record Deleted Successfully..");
+			
+		}catch (Exception e) {
+		     if (tx!=null) tx.rollback();
+		     throw e;
+		}finally {
+			 session.close();
+			//sf.close();
+		 }
 		return 0;
 	}
 	
 	public static Student search()
 	{
-		System.out.println("Enter ID :");
-		int id=sc.nextInt();
-		
-		session=sf.openSession();
-		student=session.get(Student.class,id);
-		
-		session.close();
-		//sf.close();	
-		
+		try {
+			System.out.println("Enter ID :");
+			int id=sc.nextInt();
+			
+			session=sf.openSession();
+			student=session.get(Student.class,id);
+			
+		}catch (Exception e) {
+		     if (tx!=null) tx.rollback();
+		     throw e;
+		}finally {
+			 session.close();
+			//sf.close();
+		 }
 		return student;
 	}
 	
 	public static List<Student> getAllRecord()
 	{
-		session=sf.openSession();
-		Criteria criteria=session.createCriteria(Student.class);
-		List<Student> slist=criteria.list();
+		List<Student> slist = null;
+		try {
+			
+			session=sf.openSession();
+			Criteria criteria=session.createCriteria(Student.class);
+			slist=criteria.list();
 		
-		session.close();
-		//sf.close();
-		
+		}catch (Exception e) {
+		     if (tx!=null) tx.rollback();
+		     throw e;
+		}finally {
+			 session.close();
+			//sf.close();
+		}
 		return slist;
 		
 	}
